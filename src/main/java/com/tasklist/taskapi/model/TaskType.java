b97c8622f33d622dffacfd4e6f55260327717eb4
Ella Nikola Barkane <eb23122@edu.lu.lv>
2025-06-10 16:30:39 +0300

    Update TaskType enum values

--- src/main/java/com/tasklist/taskapi/model/TaskType.java
+++ src/main/java/com/tasklist/taskapi/model/TaskType.java
@@ -1,9 +1,9 @@
 package com.tasklist.taskapi.model;
 
 public enum TaskType {
-    PERSONAL,
-    WORK,
-    STUDY,
-    HEALTH,
-    OTHER
+    EPIC,
+    STORY,
+    TASK,
+    SUBTASK,
+    BUG
 }
