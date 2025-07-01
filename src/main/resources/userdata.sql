eca972843692a07e9c94e432e8c3af3fb04048ab
Ella Nikola Barkane <eb23122@edu.lu.lv>
2025-07-01 20:34:56 +0300

    TM-31: Add USERS table, backend API and FE user list page

---
+++ src/main/resources/userdata.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (username, password, first_name, last_name) VALUES
+  ('johnsmith', 'pass123', 'John', 'Smith'),
+  ('annajones', 'secure456', 'Anna', 'Jones'),
+  ('petrov', 'abc789', 'Ivan', 'Petrov');
