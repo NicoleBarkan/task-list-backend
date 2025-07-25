64fb13bbe53b70ca5a43159bbfb2f6f0aceb4022
Ella Nikola Barkane <eb23122@edu.lu.lv>
2025-07-25 17:26:59 +0300

    TM-41: Add user details and role update functionality for admin

--- src/main/java/com/tasklist/taskapi/controller/UserController.java
+++ src/main/java/com/tasklist/taskapi/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.tasklist.taskapi.controller;
 
+import com.tasklist.taskapi.model.Role;
 import com.tasklist.taskapi.model.User;
 import com.tasklist.taskapi.service.UserService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/api/users")
@@ -18,5 +21,22 @@ public class UserController {
     @GetMapping
     public List<User> getAllUsers() {
         return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        return userService.getUserById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}/role")
+    public ResponseEntity<User> updateRole(@PathVariable Long id, @RequestBody Set<Role> newRole) {
+        return userService.getUserById(id).map(user -> {
+            newRole.add(Role.USER);
+            user.setRole(newRole);
+            userService.saveUser(user);
+            return ResponseEntity.ok(user);
+        }).orElse(ResponseEntity.notFound().build());
     }
 }
--- src/main/java/com/tasklist/taskapi/service/UserService.java
+++ src/main/java/com/tasklist/taskapi/service/UserService.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 public interface UserService {
     List<User> getAllUsers();
     Optional<User> getUserById(Long id);
+    void saveUser(User user);
 }
