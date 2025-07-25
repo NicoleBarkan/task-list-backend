7ed6a1430c9457a51969a989d5d67fcea7daea86
Ella Nikola Barkane <eb23122@edu.lu.lv>
2025-07-25 17:24:47 +0300

    TM-40: Add Role enum and user role set to User model

---
+++ src/main/java/com/tasklist/taskapi/model/Role.java
@@ -0,0 +1,7 @@
+package com.tasklist.taskapi.model;
+
+public enum Role {
+    ADMIN,
+    MANAGER,
+    USER
+}
--- src/main/java/com/tasklist/taskapi/model/User.java
+++ src/main/java/com/tasklist/taskapi/model/User.java
@@ -4,6 +4,9 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
 @Table(name = "users")
 @Getter
@@ -24,4 +27,13 @@ public class User {
 
     @Column(name = "last_name", nullable = false)
     private String lastName;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(
+        name = "user_role",
+        joinColumns = @JoinColumn(name = "user_id")
+    )
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role")
+    private Set<Role> role = new HashSet<>();
 }
